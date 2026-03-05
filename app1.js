50f7d5a545cb3fde33b7d456d34e7022fa5c1b22
Bhavik <bhavikgandhi138@gmail.com>
2026-03-05 23:55:43 +0530

    Update

--- app1.js
+++ app1.js
@@ -1,17 +1,30 @@
 function seoTitle() {
-  let title = document.getElementById("title");
+  let title = document.getElementById("title").value;
+  let length = title.length;
 
-  let titleResult = "Meta Title count is " + title.length;
+  document.getElementById("titleResult").innerText = "Meta Title count is " + length;
+  // let titleResult = "Meta Title count is " + title.length;
   //   console.log(titleResult.length);
-  let resultDiv = document.getElementById("titleResult");
-  resultDiv.innerText = titleResult;
+  // let resultDiv = document.getElementById("titleResult");
+  // resultDiv.innerText = titleResult;
 }
 
-function seoDesc() {
-  let desc = document.getElementById("desc");
+function countLetters() {
+  let desc = document.getElementById("desc").value;
+  document.getElementById("descResult").innerText = "Meta Description is " + desc.length;
 
-  let descResult = "Meta Description count is " + desc.length;
-  //   console.log(titleResult.length);
-  let resultDiv = document.getElementById("descResult");
-  resultDiv.innerText = descResult;
+  if (desc.length < 120) {
+    descRange.innerText = "Need to Improve - Too Short";
+    descRange.className = "text-red-500 font-medium";
+  } else if (desc.length <= 160) {
+    descRange.innerText = "Optimal";
+    descRange.className = "text-green-500 font-medium";
+  } else {
+    descRange.innerText = "Need to Optimize Meta Description - Too Long";
+    descRange.className = "text-amber-500 font-medium";
+  }
+  // let descResult = "Meta Description count is " + desc.length;
+  //  console.log(titleResult.length);
+  // let resultDiv = document.getElementById("descResult");
+  // resultDiv.innerText = descResult;
 }
